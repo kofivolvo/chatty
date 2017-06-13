d962819c9dedbe8456803ce1740b752d74d1aac2
Ian Osei-Agyeman <ianosei-agyeman@Ians-MacBook-Pro.local>
2017-06-13 15:01:32 -0500

    Still have to do black diamond

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const express = require('express')
 
+const bodyParser= require('body-parser')
+
+const app = express();
+
+app.listen(3000, function(){
+  console.log('The Swagdoodle Of All Swagdoodles');
+})
+
+app.use(express.static('assest'))
+app.use(bodyParser.json())
+
+const messages = [
+ {
+   messages: 'This Is Message',
+   message: 'Wub Dub Ab Luh Do Da Doo'
+}
+]
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json({ messages:messages })
+
+})
+
+app.post('/messages', function(req, res, next){
+  //console.log(req.body);
+  //console.log(req.body.message);
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages })
+})
